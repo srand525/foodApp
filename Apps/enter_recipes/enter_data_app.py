044b21f71899e15fdcea4d4594cd60203709a67f
Sophie Rand <s.rand525@gmail.com>
2018-03-09 15:43:59 -0500

    swap create for merge

--- Apps/enter_recipes/enter_data_app.py
+++ Apps/enter_recipes/enter_data_app.py
@@ -10,6 +10,10 @@ from neo4j.v1 import GraphDatabase
 
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return render_template('intface.html')
+
 def connect_to_db(graph_name):
     graph_cnxn = Graph(graph_name)
     return graph_cnxn
@@ -22,17 +26,23 @@ def push_new_component(component_name):
     graph_cnxn.run(add_component_query)
     print(add_component_query)
 
+# def push_new_ingredient(ingredient_name):
+#     # my_graph = Graph('bolt://127.0.0.1:7687')
+#     graph_cnxn = connect_to_db('bolt://127.0.0.1:7687')
+#     ingredient_format = "'" + ingredient_name + "'"
+#     add_ingredient_query = """CREATE (i:ingredient {{name: {}}})""".format(ingredient_format)
+#     graph_cnxn.run(add_ingredient_query)
+#     print(add_ingredient_query)
+
 def push_new_ingredient(ingredient_name):
     # my_graph = Graph('bolt://127.0.0.1:7687')
     graph_cnxn = connect_to_db('bolt://127.0.0.1:7687')
     ingredient_format = "'" + ingredient_name + "'"
-    add_ingredient_query = """CREATE (i:ingreident {{name: {}}})""".format(ingredient_format)
-    graph_cnxn.run(add_ingredient_query)
-    print(add_ingredient_query)
+    # add_ingredient_query = """CREATE (i:ingredient {{name: {}}})""".format(ingredient_format)
+    merge_ingredient_query = """MERGE (i:ingredient {{name:{}}})""".format(ingredient_format)
+    graph_cnxn.run(merge_ingredient_query)
+    print(merge_ingredient_query)
 
-@app.route("/")
-def main():
-    return render_template('intface.html')
 
 @app.route("/addIngredient",methods=['GET', 'POST'])
 def addIngredient():
